1e38f557cd63e53284db381a1f7aa7910ee971b4
Jules <jbraun3@uw.edu>
2026-04-27 20:51:56 -0700

    Oops FavoriteItem fix

--- WSDOT-iOS-iDOT/WSDOT_iOS_iDOTApp.swift
+++ WSDOT-iOS-iDOT/WSDOT_iOS_iDOTApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct WSDOT_iOS_iDOTApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            FavoriteItem.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
