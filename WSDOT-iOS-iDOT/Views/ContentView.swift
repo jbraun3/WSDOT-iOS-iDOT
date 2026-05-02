96f01abc4ffae9619736eec98ac127bdb6bdea61
dinerodan17 <dbejar17@uw.edu>
2026-05-02 15:03:59 -0700

    resolved merge conflicts + home icons + routing

--- WSDOT-iOS-iDOT/Views/ContentView.swift
+++ WSDOT-iOS-iDOT/Views/ContentView.swift
@@ -11,31 +11,46 @@ import SwiftData
 struct ContentView: View {
     
     //Traffic Map, Ferries, Mountain Passes, Bridge Alerts, Toll Rates, Border Waits, Amtrak, My Routes
-    let icons = ["map.fill", "ferry.fill", "mountain.2.fill", "person.fill", "creditcard.fill", "flag.fill", "tram.fill", "road.lanes"]
+    let icons = ["icHomeTraffic", "icHomeFerries", "icHomePasses", "icBridgeAlerts", "icHomeTollRates", "icHomeBorderWaits", "icHomeAmtrakCascades", "icHomeMyRoutes"]
     let labels = ["Traffic Map", "Ferries", "Mountain Passes", "Bridge Alerts", "Toll Rates", "Border Waits", "Amtrak", "My Routes"]
     
     let radius: CGFloat = 120
     
     var body: some View {
-        ZStack{
-            ForEach(0 ..< icons.count, id :\.self){index in
-                let angle = Double(index) * (360/Double(icons.count))
-                VStack{
-                    Image(systemName: icons[index])
-                        .font(.system(size:60))
-                        .foregroundColor(.wsdoTprimarygreen)
-                        .frame(width: 50, height: 50)
-                    Text(labels[index])
-                        .font(.caption)
+        NavigationView {
+            ZStack{
+                ForEach(0 ..< icons.count, id :\.self){index in
+                    let angle = Double(index) * (360/Double(icons.count))
+                    NavigationLink(destination: destinations[index]) {
+                        VStack{
+                            Image(icons[index])
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                            Text(labels[index])
+                                .font(.caption)
 
+                        }
+                    }
+                    .buttonStyle(.plain)
+                    .rotationEffect(.degrees(-angle))
+                    .offset(y: -radius)
+                    .rotationEffect(.degrees(angle))
                 }
-                .rotationEffect(.degrees(-angle))
-
-                .offset(y: -radius)
-                .rotationEffect(.degrees(angle))
             }
         }
     }
+    
+    let destinations: [AnyView] = [
+        AnyView(TrafficMapHome()),
+        AnyView(FerriesHome()),
+        AnyView(MountainPassesHome()),
+        AnyView(BridgeAlertsHome()),
+        AnyView(TollRatesHome()),
+        AnyView(BorderWaitsHome()),
+        AnyView(AmtrakHome()),
+        AnyView(MyRoutesHome())
+    ]
 }
 
 
