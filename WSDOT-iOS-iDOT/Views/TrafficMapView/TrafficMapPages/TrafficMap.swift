ef8cd47eeb3dd64683220fd1a19570c5e2aa7d2c
Jules <jbraun3@uw.edu>
2026-05-02 15:26:12 -0700

    Api key file

---
+++ WSDOT-iOS-iDOT/Views/TrafficMapView/TrafficMapPages/TrafficMap.swift
@@ -0,0 +1,13 @@
+//
+//  TrafficMapLocation.swift
+//  WSDOT-iOS-iDOT
+//
+//  Created by Jules on 4/29/26.
+//
+import SwiftUI
+
+struct TrafficMapLocation: View {
+    var body: some View {
+        Text("TrafficMapLocation")
+    }
+}
