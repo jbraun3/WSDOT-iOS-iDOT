ab678eb9c379a1a31df15214529a4c9b7723fa7d
dinerodan17 <dbejar17@uw.edu>
2026-05-04 20:04:21 -0700

    traffic map

--- WSDOT-iOS-iDOT/Views/TrafficMapView/TrafficMapPages/TrafficMap.swift
+++ WSDOT-iOS-iDOT/Views/TrafficMapView/TrafficMapPages/TrafficMap.swift
@@ -1,13 +1,22 @@
 //
-//  TrafficMapLocation.swift
+//  TrafficMap.swift
 //  WSDOT-iOS-iDOT
 //
 //  Created by Jules on 4/29/26.
 //
 import SwiftUI
+import MapKit
 
 struct TrafficMap: View {
+    @State private var position: MapCameraPosition = .region(MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 47.6062, longitude: -122.3321),
+        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    ))
+    
     var body: some View {
-        Text("TrafficMapLocation")
+        Map(position: $position) {
+            UserAnnotation()
+        }
+        .mapStyle(.standard)
     }
 }
